d1ba43277b1b64b4eeff060e997c63172ef8e90e
Armen Donigian <donigian@gmail.com>
2016-05-10 01:10:05 -0700

    Multiple file folder structure.

---
+++ code/Multiple File Structure/test/geometric_utils_test.cpp
@@ -0,0 +1,11 @@
+//
+// Created by arm on 5/10/16.
+//
+#include <assert.h>
+
+#include "../include/geometric_utils.h"
+
+
+void test_calc_area_of_square(double side) {
+    assert( calc_area_of_square(2.0) == 4.0 );
+}
---
+++ labs/lab1.md
@@ -0,0 +1,4 @@
+Implement a calculator which supports addition, subtraction, multiplication and
+division using multiple file structure. 
+
+Please make sure you do sufficient testing.
