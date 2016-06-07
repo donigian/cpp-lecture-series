ee7f5b419dc5de9a43089a01d4e6c33c7b57cb80
Armen Donigian <donigian@gmail.com>
2016-06-06 23:17:50 -0700

    Exercise 1.

--- code/Hello World/main_cpp_11.cpp
+++ code/Hello World/main_cpp_11.cpp
@@ -5,7 +5,11 @@
 // To compile: g++ -o hello_11 main_11.cpp -std=c++11
 #include <iostream>
 
+// constant guarantees compile time evaluation of an expression
 constexpr auto greeting = "hello world!";
+
+// lambda function: an annonymous way to define function object. Can be defined & passed.
+//  itermreturn string, [] represents a capture (not capturing anything from env)
 auto another_greeting = [] (std::string name) { return std::string("hello ") + name; };
 
 int main() {
